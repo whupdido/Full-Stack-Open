fff559f0685a5be3d5f2e76c0873693a2fbb2bb0
whupdido <88324453+whupdido@users.noreply.github.com>
2025-09-09 23:28:33 +0800

    remove cors

--- part3/backprog/index.js
+++ part3/backprog/index.js
@@ -1,7 +1,6 @@
 const http = require('http')
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 const app = express()
 
 morgan.token('type', function (req, res) { return JSON.stringify(req.body)})
@@ -27,7 +26,6 @@ let persons =[
       "number": "39-23-6423122"
     }
 ]
-app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
 app.get('/api/persons', (request, response) => {

48bd0aa1f655e420545b9eab7ac85313e01ebe92
whupdido <88324453+whupdido@users.noreply.github.com>
2025-09-09 23:38:10 +0800

    back to normal

--- part3/backprog/index.js
+++ part3/backprog/index.js
@@ -32,7 +32,7 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
-app.get('/', (request, response) => {
+app.get('/info', (request, response) => {
   response.send(`<p>The Phonebook has the information of ${persons.length} people</p> 
     <p>${new Date()}</p>` )
 })

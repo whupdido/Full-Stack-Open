1eefd3cd3176f1a6ccc237cf3c162e01e75173a7
whupdido <88324453+whupdido@users.noreply.github.com>
2025-09-15 01:49:32 +0800

    g

--- part3/phonebook/src/App.jsx
+++ part3/phonebook/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import personService from './services/persons'
 
 const Filter = ({ filterName, handleFilter }) => {
@@ -9,7 +8,6 @@ const Filter = ({ filterName, handleFilter }) => {
     </div>
   )
 }
-
 
 const PersonForm = ({ newName, newNumber, handleNameChange, handleNumberChange, handleSubmit }) => {
   return (
@@ -26,6 +24,7 @@ const PersonForm = ({ newName, newNumber, handleNameChange, handleNumberChange, 
     </form>
   )
 }
+
 const Notification = ({ message }) => {
   if (message === null) {
     return null
@@ -37,7 +36,6 @@ const Notification = ({ message }) => {
     </div>
   )
 }
-
 
 const Persons = ({ persons, setPersons}) => {
   const handleDel = (person) => {
@@ -61,55 +59,79 @@ const Persons = ({ persons, setPersons}) => {
   )
 }
 
-
 const App = () => {
   const [persons, setPersons] = useState([])
   const [filterName, setFilter] = useState('')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  const [errorMessage, setErrorMessage] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
-  personService.getAll().then(response => {
+    personService.getAll().then(response => {
       setPersons(response.data)
     })
-}, [])
+  }, [])
 
+  useEffect(() => {
+    if (errorMessage !== null) {
+      const timer = setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+      return () => clearTimeout(timer)
+    }
+  }, [errorMessage])
 
   const handleSubmit = (event) => {
     event.preventDefault()
     const newper = { name: newName, number: newNumber }
 
-    if ((persons.some(person => person.name === newper.name)) && window.confirm(`Replace ${newper.name} old number with new one?`)) {
-        const found = persons.find(person => person.name === newper.name)
-        const updatedPer = {...found, number:newNumber}
-        personService.update(found.id, updatedPer)
-        .then(response => {
-          setPersons(persons.map(person => person.id === found.id ? response.data : person))
-        }
-      )
-      .catch(error=>{setErrorMessage(`Information of ${newper.name} has already been removed from the server`)})
-      setErrorMessage(`Updated ${newper.name} number to ${newNumber}`)
-  }
-  else{
-    personService.create(newper)
-    .then(response => {
-    setPersons(persons.concat(response.data))
-    setNewName('')
-    setNewNumber('')
-    setErrorMessage(`Added ${newper.name}`)
-    })
-    .catch(error=>{
-      console.log(error.response.data.error)
-      setErrorMessage(`${error.response.data.error}`)})
-  }
+    const existingPerson = persons.find(person => person.name === newper.name)
     
+    if (existingPerson) {
+      if (window.confirm(`Replace ${newper.name}'s old number with new one?`)) {
+        const updatedPer = {...existingPerson, number: newNumber}
+        personService.update(existingPerson.id, updatedPer)
+          .then(response => {
+            setPersons(persons.map(person => 
+              person.id === existingPerson.id ? response.data : person
+            ))
+            setErrorMessage(`Updated ${newper.name}'s number to ${newNumber}`)
+            setNewName('')
+            setNewNumber('')
+          })
+          .catch(error => {
+            if (error.response && error.response.data && error.response.data.error) {
+              setErrorMessage(error.response.data.error)
+            } else if (error.response && error.response.status === 404) {
+              setErrorMessage(`Information of ${newper.name} has already been removed from the server`)
+              setPersons(persons.filter(p => p.id !== existingPerson.id))
+            } else {
+              setErrorMessage('An error occurred while updating the person')
+            }
+          })
+      }
+    } else {
+      personService.create(newper)
+        .then(response => {
+          setPersons(persons.concat(response.data))
+          setNewName('')
+          setNewNumber('')
+          setErrorMessage(`Added ${newper.name}`)
+        })
+        .catch(error => {
+          if (error.response && error.response.data && error.response.data.error) {
+            console.log('Validation error:', error.response.data.error)
+            setErrorMessage(error.response.data.error)
+          } else {
+            setErrorMessage('An error occurred while adding the person')
+          }
+        })
+    }
   }
 
   const handleNameChange = (event) => setNewName(event.target.value)
   const handleNumberChange = (event) => setNewNumber(event.target.value)
   const handleFilter = (event) => setFilter(event.target.value)
-
 
   const namesToShow = filterName
     ? persons.filter(person =>

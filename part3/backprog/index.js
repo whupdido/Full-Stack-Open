12ea4617abba47b176cf05b738c6bf6f22961031
whupdido <88324453+whupdido@users.noreply.github.com>
2025-09-15 01:43:25 +0800

    i

--- part3/backprog/index.js
+++ part3/backprog/index.js
@@ -11,7 +11,7 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  }else if (error.name === 'ValidationError'){ 
+  } else if (error.name === 'ValidationError') { 
     return response.status(400).json({ error: error.message })  
   }
 
@@ -23,27 +23,27 @@ app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
 
 
-app.get('/info', (request, response) => {
+app.get('/info', (request, response, next) => {
   Person.find({}).then(people => {
     response.send(`<p>The Phonebook has the information of ${people.length} people</p> 
     <p>${new Date()}</p>` )
   })
-  
+  .catch(error => next(error))
 })
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
   Person.find({}).then(people => {
     response.json(people)
   })
+  .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  Person.findById(request.params.id).then(people => {
-    if(people){
-    response.json(people)}
-    else{
-      response.status(404).end
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id).then(person => {
+    if(person){
+      response.json(person)
+    } else {
+      response.status(404).end()
     }
   })
   .catch(error => next(error))
@@ -53,40 +53,46 @@ app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then(result => {
       response.status(204).end()
-    }).catch(error => next(error))
-})
-app.put('/api/persons/:id', (request, response, next) =>{
-  const {name, number} = request.body
-  Person.findById(request.params.id)
-  .then(person =>{
-    if(!person){
-      return response.status(404).end()
-    }
-    person.name = name
-    person.number = number
-    return person.save().then((updatedPerson) =>{
-      response.json(updatedPerson)
     })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const {name, number} = request.body
+  
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' }
+  )
+  .then(updatedPerson => {
+    if (updatedPerson) {
+      response.json(updatedPerson)
+    } else {
+      response.status(404).end()
+    }
   })
   .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response, next)=>{
-const body = request.body
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
 
   const person = new Person({
     name: body.name,
     number: body.number
   })
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
-  .catch(error => next(error))
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
-
+// Error handler middleware must be last
 app.use(errorHandler)
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)

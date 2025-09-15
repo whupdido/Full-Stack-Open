cc0c919e9441a23dccd418628095a506482cc6fc
whupdido <88324453+whupdido@users.noreply.github.com>
2025-09-15 18:00:46 +0800

    part 3 done?

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,12 @@ export default [
     rules: {       '@stylistic/js/indent': ['error', 2],
             '@stylistic/js/linebreak-style': ['error', 'unix'],
                   '@stylistic/js/quotes': ['error', 'single'],
-                        '@stylistic/js/semi': ['error', 'never'],    }, 
+                        '@stylistic/js/semi': ['error', 'never'],  
+                      eqeqeq: 'error',
+                      'no-trailing-spaces': 'error',    
+                      'object-curly-spacing': ['error', 'always'],    
+                      'arrow-spacing': ['error', { before: true, after: true }],
+                          'no-console': 'off',  }, 
     files: ['**/*.js'],
     languageOptions: {
       sourceType: 'commonjs',
--- part3/backprog/index.js
+++ part3/backprog/index.js
@@ -4,15 +4,15 @@ const morgan = require('morgan')
 const Person = require('./models/person')
 const app = express()
 
-morgan.token('type', function (req, res) { return JSON.stringify(req.body)})
+morgan.token('type', function (req) { return JSON.stringify(req.body)})
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') { 
-    return response.status(400).json({ error: error.message })  
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -28,14 +28,14 @@ app.get('/info', (request, response, next) => {
     response.send(`<p>The Phonebook has the information of ${people.length} people</p> 
     <p>${new Date()}</p>` )
   })
-  .catch(error => next(error))
+    .catch(error => next(error))
 })
 
 app.get('/api/persons', (request, response, next) => {
   Person.find({}).then(people => {
     response.json(people)
   })
-  .catch(error => next(error))
+    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
@@ -46,33 +46,33 @@ app.get('/api/persons/:id', (request, response, next) => {
       response.status(404).end()
     }
   })
-  .catch(error => next(error))
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
-    .then(result => {
+    .then(() => {
       response.status(204).end()
     })
     .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const {name, number} = request.body
-  
+  const { name, number } = request.body
+
   Person.findByIdAndUpdate(
     request.params.id,
     { name, number },
     { new: true, runValidators: true, context: 'query' }
   )
-  .then(updatedPerson => {
-    if (updatedPerson) {
-      response.json(updatedPerson)
-    } else {
-      response.status(404).end()
-    }
-  })
-  .catch(error => next(error))
+    .then(updatedPerson => {
+      if (updatedPerson) {
+        response.json(updatedPerson)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {

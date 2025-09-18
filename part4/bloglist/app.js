8f632ac9ca993a3f51535db3181b819b0194b8fe
whupdido <88324453+whupdido@users.noreply.github.com>
2025-09-18 19:19:24 +0800

    f

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,25 +1,24 @@
 const express = require('express')
 const Blog = require('./models/blog')
-
+const mongoose = require('mongoose')
+const config = require('./utils/configs')
 
 const app = express()
+
+mongoose.connect(config.MONGODB_URI)
 app.use(express.json())
 
 
-app.get('/api/blogs', (request, response) => {
-Blog.find({}).then(blogs => {
+app.get('/api/blogs', async (request, response) => {
+const blogs = await Blog.find({})
 response.json(blogs)
 })
-})
 
 
-app.post('/api/blogs', (request, response) => {
+app.post('/api/blogs', async (request, response) => {
 const blog = new Blog(request.body)
-
-
-blog.save().then(result => {
+const savedBlog = await blog.save()
 response.status(201).json(result)
-})
 })
 
 
